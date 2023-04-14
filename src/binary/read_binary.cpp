a2091aeff665f62b879495d4592e063cd79d889f
Aymeric Pellé <aympelle@gmail.com>
2023-04-14 22:59:58 +0200

    v0.3.0: read_binary(char*, n) and write_binary(const char*, n) do not use rdbuf directly anymore.

--- src/binary/read_binary.cpp
+++ src/binary/read_binary.cpp
@@ -5,10 +5,6 @@ namespace seri
 {
 std::istream& read_bytes(std::istream& stream, void* buffer, std::size_t number_of_bytes_to_read)
 {
-    const std::size_t read_bytes = stream.rdbuf()->sgetn(reinterpret_cast<char*>(buffer), number_of_bytes_to_read);
-    if (read_bytes != number_of_bytes_to_read)
-        throw std::runtime_error("Failed to read " + std::to_string(number_of_bytes_to_read)
-                                 + " bytes from input stream! Read " + std::to_string(read_bytes));
-    return stream;
+    return stream.read(reinterpret_cast<char*>(buffer), number_of_bytes_to_read);
 }
 }
